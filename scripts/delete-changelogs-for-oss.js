14b8ce8425f93b30e43738514fff4a54014b69d2
Chris DeMartini <74268675+chris-demartini@users.noreply.github.com>
2021-04-02 09:55:33 -0700

    chore: add yarn audit command across repo, add changelog node script

---
+++ scripts/delete-changelogs-for-oss.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2021 Visa, Inc.
+ *
+ * This source code is licensed under the MIT license
+ * https://github.com/visa/visa-chart-components/blob/master/LICENSE
+ *
+ **/
+const path = require('path');
+const glob = require('glob');
+const fs = require('fs');
+
+const changelogFiles = [
+  ...glob.sync(path.join(__dirname, '..', 'packages') + '/*/CHANGELOG.md', {}), // package level results
+  ...glob.sync(path.join(__dirname, '..') + '/CHANGELOG.md', {}) // root results
+];
+changelogFiles.forEach(function(file, i) {
+  try {
+    fs.unlinkSync(file);
+    console.log(`SUCCESS:: file:${file} was successfully removed`);
+  } catch (ex) {
+    console.log(`Error:: while processing ts file:${file}, exception:${ex}`);
+  }
+});
+console.log(`Done removing ${changelogFiles.length} CHANGELOG.md files`);
