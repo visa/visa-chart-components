2ee635fff5db4ee24439324e9e86dd4fef52feb6
Chris DeMartini <74268675+chris-demartini@users.noreply.github.com>
2021-11-17 21:21:47 -0800

    chore: update @visa/charts build within charts-R post release 6.0.0

---
+++ packages/charts-R/inst/htmlwidgets/lib/visa-chart-components/p-lwdo8skm.system.entry.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) 2021 Visa, Inc.
+ *
+ * This source code is licensed under the MIT license
+ * https://github.com/visa/visa-chart-components/blob/master/LICENSE
+ *
+ **/
+System.register(["./p-3a3b8b8a.system.js"],(function(l){"use strict";var t,n;return{setters:[function(l){t=l.r;n=l.h}],execute:function(){var r=l("visa_charts",function(){function l(l){t(this,l)}l.prototype.render=function(){return n("div",null,n("alluvial-diagram",null),n("bar-chart",null),n("line-chart",null),n("scatter-plot",null),n("pie-chart",null),n("stacked-bar-chart",null),n("clustered-bar-chart",null),n("circle-packing",null),n("heat-map",null),n("parallel-plot",null),n("dumbbell-plot",null),n("world-map",null))};return l}())}}}));
