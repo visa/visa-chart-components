a4caa780fb2017f06d5cebf767e4e780117d70ee
Chris DeMartini <74268675+chris-demartini@users.noreply.github.com>
2022-01-20 17:01:33 -0800

    test: add fake test to force fail, update snapshots for legend fix

--- packages/bivariate-mapbox-map/src/components/bivariate-mapbox-map/bivariate-mapbox-map.spec.ts
+++ packages/bivariate-mapbox-map/src/components/bivariate-mapbox-map/bivariate-mapbox-map.spec.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020 Visa, Inc.
+ * Copyright (c) 2020, 2022 Visa, Inc.
  *
  * This source code is licensed under the MIT license
  * https://github.com/visa/visa-chart-components/blob/master/LICENSE
@@ -20,5 +20,14 @@ describe('bivariate-mapbox-map', () => {
         html: '<bivariate-mapbox-map></bivariate-mapbox-map>'
       });
     });
+  });
+});
+
+// currently we have a bug where the test script will hang and not close properly
+// if all tests pass, until this is resolved, we force fail a test at the end of the script
+// to ensure tests close out and
+describe('fake-test-fail-exit', () => {
+  it('force fail to get tests to close jest testing script when all pass', () => {
+    expect(false).toBeTruthy();
   });
 });
